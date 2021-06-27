3eac6fce8ffc96dad5f86aead2831d12400c73a5
Caio Cesar <kyle@Kyles-MacBook-Air.local>
2021-06-26 22:10:26 -0400

    add base code

---
+++ pong-gametest Shared/GameScene.swift
@@ -0,0 +1,54 @@
+//
+//  GameScene.swift
+//  test Shared
+//
+//  Created by Kyle on 6/26/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var player = SKSpriteNode()
+    
+    
+    fileprivate var label : SKLabelNode?
+    fileprivate var spinnyNode : SKShapeNode?
+
+    
+    class func newGameScene() -> GameScene {
+        // Load 'GameScene.sks' as an SKScene.
+        guard let scene = SKScene(fileNamed: "GameScene") as? GameScene else {
+            print("Failed to load GameScene.sks")
+            abort()
+        }
+        
+        // Set the scale mode to scale to fit the window
+        scene.scaleMode = .aspectFill
+        
+        return scene
+    }
+    
+    override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        player = self.childNode(withName: "player") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20 ))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame )
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border  
+        
+      
+    }
+   
+
+
+}
---
+++ pong-gametest macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  pong-gametest macOS
+//
+//  Created by Kyle on 6/26/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
